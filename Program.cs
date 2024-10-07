8c29670b7b736cfb3fac8ed72be1d1f745e4abc6
Nguyen Manh Hung <HungNMHE187081@fpt.edu.vn>
2024-10-07 16:28:11 +0700

    add bugs

--- Program.cs
+++ Program.cs
@@ -25,10 +25,29 @@ class Program
 
         // Example of a potential issue: Method with too many parameters
         MethodWithTooManyParameters(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Example of a potential issue: Null reference exception
+        string nullString = null;
+        Console.WriteLine(nullString.Length);
+
+        // Example of a potential issue: Unused method
+        UnusedMethod();
+
+        // Example of a potential issue: Infinite loop
+        while (true)
+        {
+            // Do nothing
+        }
     }
 
     static void MethodWithTooManyParameters(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
     {
         // Do something
+    }
+
+    // Example of a potential issue: Unused method
+    static void UnusedMethod()
+    {
+        // Do nothing
     }
 }
