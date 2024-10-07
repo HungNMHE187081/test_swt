9e9227cab1d5a45eef0fa7a12182c56d0ccdb876
Nguyen Manh Hung <HungNMHE187081@fpt.edu.vn>
2024-10-07 16:11:17 +0700

    test 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Example of a potential issue: Unused variable
+        int unusedVariable = 42;
+
+        // Example of a potential issue: Hardcoded password
+        string password = "12345";
+
+        // Example of a potential issue: Empty catch block
+        try
+        {
+            int result = 10 / int.Parse("0");
+        }
+        catch (Exception)
+        {
+            // Do nothing
+        }
+
+        // Example of a potential issue: Method with too many parameters
+        MethodWithTooManyParameters(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    static void MethodWithTooManyParameters(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+    {
+        // Do something
+    }
+}
